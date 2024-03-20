16ee60c9fb418f2723f2b101ce01e0184deace2f
Codefaa <facundogarcia1999@outlook.com>
2024-03-20 15:11:13 -0300

    cambioss

--- server/db/mongodb.js
+++ server/db/mongodb.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connection.on('open', () => console.log('Base de datos conectada'));
 
 async function connectDb({ host, port, dbName }) {
-    const uri = `mongodb://${host}:${port}/${dbName}`;
-    //const uri = process.env.MONGODB_URL;
+    // const uri = `mongodb://${host}:${port}/${dbName}`;
+    const uri = process.env.MONGODB_URL;
     await mongoose.connect(uri);
 }
 
